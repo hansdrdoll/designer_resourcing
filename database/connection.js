c00dc198991d1918ae22e6aa448e0a97427a94e2
Hans Kirkendoll <hans@hanskirkendoll.com>
2018-03-26 10:38:11 -0400

    added db env var

--- database/connection.js
+++ database/connection.js
@@ -1,6 +1,8 @@
 const pgp = require('pg-promise')({});
 
-const connectionURL = 'postgres://localhost:5432/designer_resourcing';
+// const connectionURL = 'postgres://localhost:5432/designer_resourcing';
+
+const connectionURL = process.env.DATABASE_URL || 'postgres://localhost:5432/designer_resourcing';
 
 const db = pgp(connectionURL);
 
